402ef555367fcbac7205827ebf9f4ade810ad02e
chenhao <904055767@qq.com>
2016-09-23 00:32:52 +0800

    fix sql语句写错

--- src/sql.py
+++ src/sql.py
@@ -21,7 +21,7 @@ class DataBase(object):
         story(id INT PRIMARY KEY AUTO_INCREMENT, user_icon VARCHAR(200), user_name VARCHAR(100), type INT(5), text TEXT, 
         image_url VARCHAR(200),time VARCHAR(50), content_from VARCHAR(50))"""
         cur.execute(sql)
-        save_sql = "insert into auto_priority(user_icon, user_name, , type, text, image_url, time, content_from) " \
+        save_sql = "insert into story(user_icon, user_name, type, text, image_url, time, content_from) " \
                    "values(%s, %s, %s, %s, %s, %s, %s)"
         cur.executemany(save_sql, datas)
         cur.close()
